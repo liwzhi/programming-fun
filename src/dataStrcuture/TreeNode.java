e91d3ea54502c136316271b4708eac709636a2c1
liwzhi <liwzhi@gmail.com>
2017-02-18 21:45:57 -0800

    add data strcuture

---
+++ src/dataStrcuture/TreeNode.java
@@ -0,0 +1,11 @@
+package dataStrcuture;
+
+/**
+ * Created by weizhi on 2/18/17.
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+     }
---
+++ src/depthFirstSearch/minimumDepthBinaryTree.java
@@ -0,0 +1,14 @@
+package depthFirstSearch;
+
+import dataStrcuture.TreeNode;
+
+/**
+ * Created by weizhi on 2/18/17.
+ */
+public class minimumDepthBinaryTree {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left ==null) return 1+minDepth(root.right);
+        if(root.right ==null) return 1+minDepth(root.left);
+        return 1+Math.min(minDepth(root.left),minDepth(root.right));
+    }}
